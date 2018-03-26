8820a0a853137d15015aec3434e33bcfe4ece22b
Skittles9823 <skat1@live.com.au>
2018-03-26 14:56:08 +1100

    Implement /getlink into AGBanBot
    Squash of commits from https://github.com/skittles9823/SkittBot
    
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- tg_bot/modules/special.py
+++ tg_bot/modules/special.py
@@ -6,7 +6,7 @@ from telegram import Update, Bot
 from telegram.error import BadRequest
 from telegram.ext import MessageHandler, Filters, CommandHandler
 from telegram.ext.dispatcher import run_async
-from tg_bot.modules.helper_funcs.chat_status import is_user_ban_protected
+from tg_bot.modules.helper_funcs.chat_status import is_user_ban_protected, bot_admin
 
 import tg_bot.modules.sql.users_sql as sql
 from tg_bot import dispatcher, OWNER_ID, LOGGER
@@ -74,6 +74,22 @@ def snipe(bot: Bot, update: Update, args: List[str]):
             update.effective_message.reply_text("Couldn't send the message. Perhaps I'm not part of that group?")
 
 
+@run_async
+@bot_admin
+def getlink(bot: Bot, update: Update, args: List[int]):
+    if args:
+        chat_id = int(args[0])
+    else:
+        update.effective_message.reply_text("You don't seem to be referring to a chat")
+    chat = bot.getChat(chat_id)
+    bot_member = chat.get_member(bot.id)
+    if bot_member.can_invite_users:
+        invitelink = bot.exportChatInviteLink(chat_id)
+        update.effective_message.reply_text(invitelink)
+    else:
+        update.effective_message.reply_text("I don't have access to the invite link!")
+
+
 __help__ = ""  # no help string
 
 __mod_name__ = "Special"
@@ -82,8 +98,10 @@ SNIPE_HANDLER = CommandHandler("snipe", snipe, pass_args = True, filters=CustomF
 BANALL_HANDLER = CommandHandler("banall", banall, pass_args = True, filters=Filters.user(OWNER_ID))
 QUICKSCOPE_HANDLER = CommandHandler("quickscope", quickscope, pass_args = True, filters=CustomFilters.sudo_filter)
 QUICKUNBAN_HANDLER = CommandHandler("quickunban", quickunban, pass_args = True, filters=CustomFilters.sudo_filter)
+GETLINK_HANDLER = CommandHandler("getlink", getlink, pass_args = True, filters=Filters.user(OWNER_ID))
 
 dispatcher.add_handler(SNIPE_HANDLER)
 dispatcher.add_handler(BANALL_HANDLER)
 dispatcher.add_handler(QUICKSCOPE_HANDLER)
 dispatcher.add_handler(QUICKUNBAN_HANDLER)
+dispatcher.add_handler(GETLINK_HANDLER)
